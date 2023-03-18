1c7e48bff319b003cbec0a5e2c96ad4aeeaa745d
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2023-03-17 23:43:39 -0300

    Problema com o flutter no Android

---
+++ lib/app/modules/auth/auth_module.dart
@@ -0,0 +1,25 @@
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/modules/todo_list_module.dart';
+import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
+import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
+import 'package:todo_list_provider/app/modules/auth/register/register_controller.dart';
+import 'package:todo_list_provider/app/modules/auth/register/register_page.dart';
+
+class AuthModule extends TodoListModule {
+  AuthModule()
+      : super(
+          routers: {
+            '/login': (context) => LoginPage(),
+            '/register': (context) => RegisterPage(),
+          },
+          bindings: [
+            ChangeNotifierProvider(
+              create: (_) => LoginController(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => RegisterController(),
+            ),
+          ],
+        );
+}

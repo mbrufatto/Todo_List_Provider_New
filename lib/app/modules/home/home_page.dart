cd7e121e86be700bb76d664ea1a39d7482293d87
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2023-03-21 11:00:37 -0300

    Criado o Drawer

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../../core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,14 +10,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            context.read<AuthProvider>().logout();
-          },
-          child: Text('Logout'),
-        ),
-      ),
+      drawer: HomeDrawer(),
+      body: Container(),
     );
   }
 }
